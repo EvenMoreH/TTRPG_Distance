19065c39b59e89ff785b3d2ca3f56a4f8aaec4c5
EvenMoreH <hubert.lewan@gmail.com>
2025-01-08 21:46:02 +0100

    working prototype of feet conversion

--- app/main.py
+++ app/main.py
@@ -10,6 +10,14 @@ import re
 # for local
 app, rt = fast_app(static_path="app/static") # type: ignore
 
+# TODO:
+#   1. Build remaining endpoints to convert:
+#       A. meters to ft and hex
+#       B. hexes to ft and meters
+#   2. Fix styling and layout
+#       A. .input-field needs to be added and configured within styles.css file
+#   3. Ready for Docker
+
 class Global_clean_string:
     clean_string: str
 
@@ -24,15 +32,45 @@ def string_cleaner(temperature: str):
 
 distance_form = Div(
 Div(
-    H1("TTRPG Distance"),
-    Button("Reset"),
+    H1(
+        "TTRPG Distance",
+        style="margin-bottom: 1vw;",
+    ),
+    Form(
+            Button(
+            "Reset",
+            type="submit",
+        ),
+        action="/",
+        method="get",
+        style="max-height: 0px",
+    ),
     cls="row",
 ),
 Div(
     Div(
-        Input(name="placeholder - feet"),
-        Div("conversion placeholder"),
-        Div("conversion placeholder"),
+        Input(
+            id="feet",
+            name="feet",
+            type="text",
+            placeholder="Enter feet",
+            hx_post="/convert-ft",
+            hx_trigger="input",
+            hx_include="#feet",
+            cls="input-field",
+        ),
+        Div(
+            id="results-ftm",
+            hx_swap_oob="true",
+            hx_swap="innerHTML",
+            cls="output",
+        ),
+        Div(
+            id="results-fth",
+            hx_swap_oob="true",
+            hx_swap="innerHTML",
+            cls="output",
+        ),
         cls="column",
     ),
     Div(
@@ -66,7 +104,41 @@ def homepage():
         Body(distance_form)
     )
 
+@rt("/convert-ft")
+def convert_ft(feet: str):
+    try:
+        feet_value = float(feet)
+        meters = feet_value * 0.3       # rounded to better utilize round method below
+        meters = round(meters * 2) / 2  # result will always be either .0 or .5
+        hexes = feet_value / 5          # 1 hex/sq = 1 inch which is 5 ft
+        return Div(
+            f"Meters: {meters:.1f}",
+            id="results-ftm",
+            hx_swap_oob="true",
+            hx_swap="innerHTML",
+            cls="output",
+        ), Div(
+            f"Hexes: {hexes:.1f}",
+            id="results-fth",
+            hx_swap_oob="true",
+            hx_swap="innerHTML",
+            cls="output",
+        )
+    except ValueError:
+        return Div(
+        "Invalid input. Please enter a numeric value.",
+        id="results-ftm",
+        hx_swap_oob="true",
+        hx_swap="innerHTML",
+        cls="output",
+        ), Div(
+            "Invalid input. Please enter a numeric value.",
+            id="results-fth",
+            hx_swap_oob="true",
+            hx_swap="innerHTML",
+            cls="output",
+        )
 
 if __name__ == '__main__':
     # Important: Use host='0.0.0.0' to make the server accessible outside the container
-    serve(host='0.0.0.0', port=5017) # type: ignore
+    serve(host='0.0.0.0', port=5017)
